39f8b8c341f87ccd5e56e60737a50b105b951cdb
Tharinda Wickramaarachchi <tharinda.tpw@gmail.com>
2020-09-26 23:41:37 +0530

    x99:Calculation v2

--- src/main/java/com/x99me/priceEngine/controller/service/PricingEngineService.java
+++ src/main/java/com/x99me/priceEngine/controller/service/PricingEngineService.java
@@ -68,18 +68,26 @@ public class PricingEngineService
 		log.info( productDAO.toString() );
 		PriceResult priceResult = new PriceResult();
 
+		System.out.println( "Req : carton = " + carton + " unit = " + unit );
 		int newCarton = carton +  unit / productDAO.getUnitPerCarton();
 		int newUnit = unit % productDAO.getUnitPerCarton();
+
+		System.out.println( "New : carton = " + newCarton + " unit = " + newUnit );
 
 		priceResult.setCartons( newCarton );
 		priceResult.setUnits( newUnit );
 
-		BigDecimal cartonPrice = productDAO.getCartonPrice().multiply( BigDecimal.valueOf( newCarton ) )
-				.multiply( (newCarton >=3) ? BigDecimal.valueOf( 0.9 ) : BigDecimal.valueOf( 1 ) );
+		BigDecimal discountFactor = ( newCarton >= 3 ) ? BigDecimal.valueOf( 0.9 ) : BigDecimal.valueOf( 1 );
 
+		BigDecimal cartonPrice = productDAO.getCartonPrice().multiply( BigDecimal.valueOf( newCarton ) )
+				.multiply( discountFactor );
+
+		System.out.println( "Carton Price : " + cartonPrice + " | x " + discountFactor );
 		BigDecimal unitPrice = BigDecimal.valueOf( newUnit ).multiply(
 				productDAO.getCartonPrice().divide( BigDecimal.valueOf( productDAO.getUnitPerCarton() ), 2, RoundingMode.HALF_EVEN )
 		).multiply( BigDecimal.valueOf( 1.3 ) );
+
+		System.out.println( "Unit Price : " + unitPrice );
 
 		priceResult.setPrice( unitPrice.add( cartonPrice ) );
 
